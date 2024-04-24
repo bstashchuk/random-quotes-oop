79802864ea0438027bfd142c44e9a2151f66dd0d
Bogdan Stashchuk <bstashchuk@gmail.com>
2024-04-24 12:39:31 +0100

    Get random quotes via public API service

--- src/classes/RandomQuote.js
+++ src/classes/RandomQuote.js
@@ -8,6 +8,16 @@ class RandomQuote {
     const { id, text, author } = quotes[randomIndex];
     return new Quote(id, text, author);
   }
+
+  static getRandomQuoteViaAPI() {
+    const url = 'https://api.quotable.io/random';
+    const options = { headers: { 'Content-Type': 'application/json' } };
+
+    return fetch(url, options)
+      .then((response) => response.json())
+      .then(({ _id: id, content, author }) => new Quote(id, content, author))
+      .catch((error) => console.error(error));
+  }
 }
 
 export default RandomQuote;
--- src/classes/RandomQuotesApp.js
+++ src/classes/RandomQuotesApp.js
@@ -1,8 +1,10 @@
+import Quote from './Quote.js';
 import RandomQuote from './RandomQuote.js';
 
 class RandomQuotesApp {
   constructor() {
     this.randomQuoteBtn = document.getElementById('random-quote-btn');
+    this.randomQuoteAPIBtn = document.getElementById('random-quote-api-btn');
     this.quoteTextElement = document.getElementById('quote-text');
     this.quoteAuthorElement = document.getElementById('quote-author');
     this.currentQuote = null;
@@ -15,14 +17,28 @@ class RandomQuotesApp {
     this.quoteAuthorElement.textContent = this.currentQuote.formatAuthor();
   }
 
+  changeCurrentQuote(newQuote) {
+    if (newQuote instanceof Quote) {
+      this.currentQuote = newQuote;
+      this.displayCurrentQuote();
+    }
+  }
+
   getRandomQuote() {
-    const randomQuote = RandomQuote.getRandomQuote();
-    this.currentQuote = randomQuote;
-    this.displayCurrentQuote();
+    this.changeCurrentQuote(RandomQuote.getRandomQuote());
+  }
+
+  getRandomQuoteViaAPI() {
+    RandomQuote.getRandomQuoteViaAPI().then((quote) =>
+      this.changeCurrentQuote(quote)
+    );
   }
 
   init() {
     this.randomQuoteBtn.addEventListener('click', () => this.getRandomQuote());
+    this.randomQuoteAPIBtn.addEventListener('click', () =>
+      this.getRandomQuoteViaAPI()
+    );
   }
 }
 
