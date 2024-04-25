9a61629adbcc2e8c56b3d78946ecd871f29f3768
Bogdan Stashchuk <bstashchuk@gmail.com>
2024-04-25 23:34:47 +0100

    Creating own quotes API Node.js Express server with resource /quotes/random-single

---
+++ server/data/quotes.js
@@ -0,0 +1,54 @@
+const quotes = [
+  {
+    id: 'd718475e',
+    text: "Believe you can and you're halfway there.",
+    author: 'Theodore Roosevelt',
+  },
+  {
+    id: 'e452d8aa',
+    text: 'The only way to do great work is to love what you do.',
+    author: 'Steve Jobs',
+  },
+  {
+    id: 'f6487b49',
+    text: 'It does not matter how slowly you go as long as you do not stop.',
+    author: 'Confucius',
+  },
+  {
+    id: 'g3736f8e',
+    text: 'The best way to predict the future is to create it.',
+    author: 'Peter Drucker',
+  },
+  {
+    id: 'h20d4dac',
+    text: 'Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work.',
+    author: 'Steve Jobs',
+  },
+  {
+    id: 'i446d47ef',
+    text: "You don't have to be great to start, but you have to start to be great.",
+    author: 'Zig Ziglar',
+  },
+  {
+    id: 'j608d995',
+    text: 'The only limit to our realization of tomorrow will be our doubts of today.',
+    author: 'Franklin D. Roosevelt',
+  },
+  {
+    id: 'k9ac6bee8',
+    text: 'It always seems impossible until it is done.',
+    author: 'Nelson Mandela',
+  },
+  {
+    id: 'l5370aeb0',
+    text: 'Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful.',
+    author: 'Albert Schweitzer',
+  },
+  {
+    id: 'm23c35bbc',
+    text: 'The only person you are destined to become is the person you decide to be.',
+    author: 'Ralph Waldo Emerson',
+  },
+];
+
+module.exports = quotes;
---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const quotes = require('./data/quotes');
+const app = express();
+const PORT = 3000;
+
+function getRandomQuote() {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  return quotes[randomIndex];
+}
+
+app.get('/quotes/random-single', (req, res) => {
+  const randomQuote = getRandomQuote();
+  res.json(randomQuote);
+});
+
+app.listen(PORT, () => {
+  console.log(`Quotes API service is running on port ${PORT}`);
+});
